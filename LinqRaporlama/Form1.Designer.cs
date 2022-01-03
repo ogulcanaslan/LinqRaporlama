add2eb8b723760d2a4a7cc1c29163f429a6328ac
Ogulcan aslan <ogulcan.aslan@hotmail.com>
2022-01-03 20:03:34 +0300

    Add project files.

---
+++ LinqRaporlama/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace LinqRaporlama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btnFiyataGoreSirala = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btnUrunAdinaGoreSondan = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btnUrunAdinaGoreBastan = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btn2 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btn3 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btn4 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btn5 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.ActiveBorderThickness = 1;
+            this.btn1.ActiveCornerRadius = 20;
+            this.btn1.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btn1.ActiveForecolor = System.Drawing.Color.White;
+            this.btn1.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btn1.BackColor = System.Drawing.SystemColors.Control;
+            this.btn1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn1.BackgroundImage")));
+            this.btn1.ButtonText = "Buton 1";
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btn1.IdleBorderThickness = 1;
+            this.btn1.IdleCornerRadius = 20;
+            this.btn1.IdleFillColor = System.Drawing.Color.White;
+            this.btn1.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btn1.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btn1.Location = new System.Drawing.Point(71, 56);
+            this.btn1.Margin = new System.Windows.Forms.Padding(5);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(181, 54);
+            this.btn1.TabIndex = 1;
+            this.btn1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnFiyataGoreSirala
+            // 
+            this.btnFiyataGoreSirala.ActiveBorderThickness = 1;
+            this.btnFiyataGoreSirala.ActiveCornerRadius = 20;
+            this.btnFiyataGoreSirala.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btnFiyataGoreSirala.ActiveForecolor = System.Drawing.Color.White;
+            this.btnFiyataGoreSirala.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btnFiyataGoreSirala.BackColor = System.Drawing.SystemColors.Control;
+            this.btnFiyataGoreSirala.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnFiyataGoreSirala.BackgroundImage")));
+            this.btnFiyataGoreSirala.ButtonText = "Fiyata Göre Sırala";
+            this.btnFiyataGoreSirala.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFiyataGoreSirala.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFiyataGoreSirala.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnFiyataGoreSirala.IdleBorderThickness = 1;
+            this.btnFiyataGoreSirala.IdleCornerRadius = 20;
+            this.btnFiyataGoreSirala.IdleFillColor = System.Drawing.Color.White;
+            this.btnFiyataGoreSirala.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btnFiyataGoreSirala.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btnFiyataGoreSirala.Location = new System.Drawing.Point(71, 120);
+            this.btnFiyataGoreSirala.Margin = new System.Windows.Forms.Padding(5);
+            this.btnFiyataGoreSirala.Name = "btnFiyataGoreSirala";
+            this.btnFiyataGoreSirala.Size = new System.Drawing.Size(181, 54);
+            this.btnFiyataGoreSirala.TabIndex = 2;
+            this.btnFiyataGoreSirala.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnFiyataGoreSirala.Click += new System.EventHandler(this.btnFiyataGoreSirala_Click);
+            // 
+            // btnUrunAdinaGoreSondan
+            // 
+            this.btnUrunAdinaGoreSondan.ActiveBorderThickness = 1;
+            this.btnUrunAdinaGoreSondan.ActiveCornerRadius = 20;
+            this.btnUrunAdinaGoreSondan.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreSondan.ActiveForecolor = System.Drawing.Color.White;
+            this.btnUrunAdinaGoreSondan.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreSondan.BackColor = System.Drawing.SystemColors.Control;
+            this.btnUrunAdinaGoreSondan.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUrunAdinaGoreSondan.BackgroundImage")));
+            this.btnUrunAdinaGoreSondan.ButtonText = "Ürün Adına Göre Sondan Sırala";
+            this.btnUrunAdinaGoreSondan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUrunAdinaGoreSondan.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUrunAdinaGoreSondan.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreSondan.IdleBorderThickness = 1;
+            this.btnUrunAdinaGoreSondan.IdleCornerRadius = 20;
+            this.btnUrunAdinaGoreSondan.IdleFillColor = System.Drawing.Color.White;
+            this.btnUrunAdinaGoreSondan.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreSondan.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreSondan.Location = new System.Drawing.Point(71, 184);
+            this.btnUrunAdinaGoreSondan.Margin = new System.Windows.Forms.Padding(5);
+            this.btnUrunAdinaGoreSondan.Name = "btnUrunAdinaGoreSondan";
+            this.btnUrunAdinaGoreSondan.Size = new System.Drawing.Size(181, 54);
+            this.btnUrunAdinaGoreSondan.TabIndex = 3;
+            this.btnUrunAdinaGoreSondan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnUrunAdinaGoreSondan.Click += new System.EventHandler(this.btnUrunAdinaGoreSondan_Click);
+            // 
+            // btnUrunAdinaGoreBastan
+            // 
+            this.btnUrunAdinaGoreBastan.ActiveBorderThickness = 1;
+            this.btnUrunAdinaGoreBastan.ActiveCornerRadius = 20;
+            this.btnUrunAdinaGoreBastan.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreBastan.ActiveForecolor = System.Drawing.Color.White;
+            this.btnUrunAdinaGoreBastan.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreBastan.BackColor = System.Drawing.SystemColors.Control;
+            this.btnUrunAdinaGoreBastan.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUrunAdinaGoreBastan.BackgroundImage")));
+            this.btnUrunAdinaGoreBastan.ButtonText = "Ürün Adına Göre Baştan Sırala";
+            this.btnUrunAdinaGoreBastan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUrunAdinaGoreBastan.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUrunAdinaGoreBastan.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreBastan.IdleBorderThickness = 1;
+            this.btnUrunAdinaGoreBastan.IdleCornerRadius = 20;
+            this.btnUrunAdinaGoreBastan.IdleFillColor = System.Drawing.Color.White;
+            this.btnUrunAdinaGoreBastan.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreBastan.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btnUrunAdinaGoreBastan.Location = new System.Drawing.Point(71, 248);
+            this.btnUrunAdinaGoreBastan.Margin = new System.Windows.Forms.Padding(5);
+            this.btnUrunAdinaGoreBastan.Name = "btnUrunAdinaGoreBastan";
+            this.btnUrunAdinaGoreBastan.Size = new System.Drawing.Size(181, 54);
+            this.btnUrunAdinaGoreBastan.TabIndex = 4;
+            this.btnUrunAdinaGoreBastan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnUrunAdinaGoreBastan.Click += new System.EventHandler(this.btnUrunAdinaGoreBastan_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.ActiveBorderThickness = 1;
+            this.btn2.ActiveCornerRadius = 20;
+            this.btn2.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btn2.ActiveForecolor = System.Drawing.Color.White;
+            this.btn2.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btn2.BackColor = System.Drawing.SystemColors.Control;
+            this.btn2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn2.BackgroundImage")));
+            this.btn2.ButtonText = "buton 2";
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btn2.IdleBorderThickness = 1;
+            this.btn2.IdleCornerRadius = 20;
+            this.btn2.IdleFillColor = System.Drawing.Color.White;
+            this.btn2.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btn2.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btn2.Location = new System.Drawing.Point(71, 312);
+            this.btn2.Margin = new System.Windows.Forms.Padding(5);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(181, 54);
+            this.btn2.TabIndex = 5;
+            this.btn2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.ActiveBorderThickness = 1;
+            this.btn3.ActiveCornerRadius = 20;
+            this.btn3.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btn3.ActiveForecolor = System.Drawing.Color.White;
+            this.btn3.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btn3.BackColor = System.Drawing.SystemColors.Control;
+            this.btn3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn3.BackgroundImage")));
+            this.btn3.ButtonText = "Üçlü Bağlantı";
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btn3.IdleBorderThickness = 1;
+            this.btn3.IdleCornerRadius = 20;
+            this.btn3.IdleFillColor = System.Drawing.Color.White;
+            this.btn3.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btn3.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btn3.Location = new System.Drawing.Point(71, 376);
+            this.btn3.Margin = new System.Windows.Forms.Padding(5);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(181, 54);
+            this.btn3.TabIndex = 7;
+            this.btn3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.ActiveBorderThickness = 1;
+            this.btn4.ActiveCornerRadius = 20;
+            this.btn4.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btn4.ActiveForecolor = System.Drawing.Color.White;
+            this.btn4.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btn4.BackColor = System.Drawing.SystemColors.Control;
+            this.btn4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn4.BackgroundImage")));
+            this.btn4.ButtonText = "btn4";
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btn4.IdleBorderThickness = 1;
+            this.btn4.IdleCornerRadius = 20;
+            this.btn4.IdleFillColor = System.Drawing.Color.White;
+            this.btn4.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btn4.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btn4.Location = new System.Drawing.Point(71, 478);
+            this.btn4.Margin = new System.Windows.Forms.Padding(5);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(181, 41);
+            this.btn4.TabIndex = 8;
+            this.btn4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.ActiveBorderThickness = 1;
+            this.btn5.ActiveCornerRadius = 20;
+            this.btn5.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btn5.ActiveForecolor = System.Drawing.Color.White;
+            this.btn5.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btn5.BackColor = System.Drawing.SystemColors.Control;
+            this.btn5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn5.BackgroundImage")));
+            this.btn5.ButtonText = "btn5";
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btn5.IdleBorderThickness = 1;
+            this.btn5.IdleCornerRadius = 20;
+            this.btn5.IdleFillColor = System.Drawing.Color.White;
+            this.btn5.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btn5.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btn5.Location = new System.Drawing.Point(71, 529);
+            this.btn5.Margin = new System.Windows.Forms.Padding(5);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(181, 41);
+            this.btn5.TabIndex = 9;
+            this.btn5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.DodgerBlue;
+            this.dataGridView1.Location = new System.Drawing.Point(485, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(940, 431);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1507, 728);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btnUrunAdinaGoreBastan);
+            this.Controls.Add(this.btnUrunAdinaGoreSondan);
+            this.Controls.Add(this.btnFiyataGoreSirala);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Bunifu.Framework.UI.BunifuThinButton2 btn1;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnFiyataGoreSirala;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnUrunAdinaGoreSondan;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnUrunAdinaGoreBastan;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn2;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn3;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn4;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LinqRaporlama/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqRaporlama
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities islem = new NorthwindEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnFiyataGoreSirala_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers
+                        orderby urun.BirimFiyati
+                        select urun;
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void btnUrunAdinaGoreSondan_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers
+                        orderby urun.UrunAdi descending
+                        select urun;
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void btnUrunAdinaGoreBastan_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers
+                        orderby urun.UrunAdi ascending
+                        select urun;
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers
+                          join tedarikci in islem.Tedarikcilers on urun.TedarikciID equals tedarikci.TedarikciID
+                          select new
+                          {
+                              urun.UrunAdi,
+                              urun.BirimFiyati,
+                              urun.HedefStokDuzeyi,
+                              tedarikci.SirketAdi,
+                              tedarikci.Sehir,
+                              tedarikci.Telefon
+                          };
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            var sonuc = from urun in islem.Urunlers
+                        join kategori in islem.Kategorilers on urun.KategoriID
+                        equals kategori.KategoriID
+                        select new
+                        {
+                            urun.UrunAdi,
+                            urun.YeniSatis,
+                            urun.HedefStokDuzeyi,
+                            kategori.KategoriAdi,
+                            kategori.Tanimi
+                        };
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            var sonuc = from kate in islem.Kategorilers
+                        join u in islem.Urunlers on kate.KategoriID
+                        equals u.KategoriID
+                        join t in islem.Tedarikcilers on u.TedarikciID equals t.TedarikciID
+                        select new
+                        {
+                            u.UrunAdi,
+                            u.BirimdekiMiktar,
+                            kate.KategoriAdi,
+                            t.Telefon
+                        };
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            var sonuc = from personel in islem.Personellers
+                        join satis in islem.Satislars on
+                        personel.PersonelID equals satis.PersonelID
+                        group satis by personel.Adi into grup
+                        select new
+                        {
+                            personelAdi = grup.Key,
+                            toplamSatis = grup.Count()
+                        };
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            var sonuc = from detay in islem.Satis_Detaylaris
+                        
+                        group detay by detay.Satislar.SatisTarihi.Value.Year into Grup
+                        
+
+                        select new
+                        {
+                            Gelir = Grup.Sum(Satis => Satis.Miktar * Satis.BirimFiyati),
+                            Yil = Grup.Key
+                        };
+            dataGridView1.DataSource = sonuc.ToList();
+        }
+    }
+}
